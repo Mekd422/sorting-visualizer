2e96141e960362499a420c469085a8904c58e174
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-11 22:35:13 +0300

    function to render random numbers starting from min to max

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -35,4 +35,9 @@ export default class SortingVisualizer extends React.Component {
             </>
         )
     }
+
+}
+
+function randomIntFromInterval(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
