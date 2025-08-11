645ee907e592bcce6f17ba2243cdfaa9b575cf33
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-11 22:17:11 +0300

    sorting-visualizer class

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './SortingVisualizer.css'
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        }
+    }
+
+    componentDidMount(){
+        this.resetArray();
+    }
+}
